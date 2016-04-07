de6c63e6de9d495c5a391e60f6fec6b7036544fa
sherrycherish <617295898@qq.com>
2016-04-07 15:13:32 +0800

    登录悬浮窗

--- app/static/login.js
+++ app/static/login.js
@@ -1,41 +1,32 @@
 /**
  * Created by sherry on 16/4/5.
  */
-jQuery(document).ready(function($){
-    var $form_modal = $('.cd-user-modal'),
-        $form_login = $form_modal.find('#cd-login'),
-        $form_signup = $form_modal.find('#cd-signup'),
-        $form_modal_tab = $('.cd-switcher'),
-        $tab_login = $form_modal_tab.children('li').eq(0).children('a'),
-        $tab_signup = $form_modal_tab.children('li').eq(1).children('a'),
-        $main_nav = $('.main_nav');
+jQuery(document).ready(function($) {
+    var $form_modal = $('.qiushi-login-modal'),
+        $login_signup_button = $('.login_signup_button');
 
     //弹出窗口
-    $main_nav.on('click', function(event){
+    $login_signup_button.on('click', function (event) {
 
-        if( $(event.target).is($main_nav) ) {
-            // on mobile open the submenu
+        if ($(event.target).is($login_signup_button)) {
             $(this).children('ul').toggleClass('is-visible');
         } else {
-            // on mobile close submenu
-            $main_nav.children('ul').removeClass('is-visible');
-            //show modal layer
+            $login_signup_button.children('ul').removeClass('is-visible');
             $form_modal.addClass('is-visible');
-            //show the selected form
-            ( $(event.target).is('.cd-signup') ) ? signup_selected() : login_selected();
         }
 
     });
 
     //关闭弹出窗口
-    $('.cd-user-modal').on('click', function(event){
-        if( $(event.target).is($form_modal) || $(event.target).is('.cd-close-form') ) {
+    $('.qiushi-login-modal').on('click', function (event) {
+        if ($(event.target).is($form_modal) || $(event.target).is('.navbar'))  {
             $form_modal.removeClass('is-visible');
         }
     });
     //使用Esc键关闭弹出窗口
-    $(document).keyup(function(event){
-        if(event.which=='27'){
+    $(document).keyup(function (event) {
+        if (event.which == '27') {
             $form_modal.removeClass('is-visible');
         }
     });
+});
